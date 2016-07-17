1203c4f7a9aeb7e7f23906829640a4a56e63da6f
eigil-h <eigil.hysvaer@ymail.com>
2016-07-17 12:00:04 +0200

    LICENCE and README.md (for github)

---
+++ README.md
@@ -0,0 +1,17 @@
+aramv0
+======
+
+Audio Recorder And Music, minimalistic v0
+
+
+======
+Build instruction
+
+$ autoreconf -i <br/>
+$ ./configure <br/>
+$ make <br/>
+
+= dependencies =
+
+- JACK, the audio engine <br/>
+  Eg. $ sudo apt-get install libjack-jackd2-dev
--- src/main/cc/main.cc
+++ src/main/cc/main.cc
@@ -1,3 +1,21 @@
+/*
+	ARAMv0, the audio recorder and music minimalistic v0
+	Copyright (C) 2016 Eigil Hysvær
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -42,6 +60,6 @@ int main(int argc, char** argv)
 			cout << "Valid options are: " << endl;
 			break;
 	}
-	
+
 	cout << endl;
 }
