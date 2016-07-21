3b19ed151a678cef310200cb30fe793cc8e1d5c5
eigil-h <eigil.hysvaer@ymail.com>
2016-07-21 19:02:56 +0200

    double buffers

---
+++ src/main/cc/service/buffer.cc
@@ -0,0 +1,221 @@
+/*
+	ARAMv0, the minimalistic Audio Recorder And Music
+	Copyright (C) 2016 Eigil Hysvær
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "buffer.h"
+#include <iostream>
+#include <exception>
+#include <algorithm>
+
+/*
+ *  aram::service::double_buffer
+ */
+aram::service::double_buffer::double_buffer(int buffer_size) : b_idx(0), bbuf_ready(true)
+{
+	dbuf[0] = new buf_t(buffer_size);
+	dbuf[1] = new buf_t(buffer_size);
+	swap(); //Initialize the iterators!
+}
+
+aram::service::double_buffer::~double_buffer()
+{
+	delete dbuf[1];
+	delete dbuf[0];
+}
+
+aram::service::buf_t* aram::service::double_buffer::front_buffer()
+{
+	return dbuf[b_idx];
+}
+
+aram::service::buf_t* aram::service::double_buffer::back_buffer()
+{
+	return dbuf[(b_idx + 1)&1];
+}
+
+void aram::service::double_buffer::swap()
+{
+	lock_guard<mutex> guard(swap_safe);
+	++b_idx &= 1;
+
+	fbuf_itr = front_buffer()->begin();
+	bbuf_ready = true;
+}
+
+/*
+ *  aram::service::load_and_read_buffer
+ */
+aram::service::load_and_read_buffer::load_and_read_buffer(int siz) : double_buffer(siz)
+{
+}
+
+bool aram::service::load_and_read_buffer::read_front_buffer(aram::service::sample_t* to_buf, int len)
+{
+	bool did_swap;
+	buf_itr_t end_itr = front_buffer()->end();
+	if(fbuf_itr + len < end_itr)
+	{
+		copy(fbuf_itr, fbuf_itr + len, to_buf);
+		fbuf_itr += len;
+		did_swap = false;
+	}
+	else
+	{
+		int cutlen = end_itr - fbuf_itr;
+		int remaining = len - cutlen;
+		copy(fbuf_itr, end_itr, to_buf);
+		swap();
+		copy(fbuf_itr, fbuf_itr + remaining, &to_buf[cutlen]);
+		fbuf_itr += remaining;
+		did_swap = true;
+	}
+	return did_swap;
+}
+
+int aram::service::load_and_read_buffer::load_back_buffer(forward_list<istream*>& istr_list)
+{
+	if(!bbuf_ready)
+	{
+		return 0;
+	}
+	buf_itr_t end_itr;
+	buf_itr_t bbuf_itr;
+	{
+		lock_guard<mutex> guard(swap_safe);
+		end_itr = back_buffer()->end();
+		bbuf_itr = back_buffer()->begin();
+	}
+
+	int total_len = 0;
+	int fillsize = end_itr - bbuf_itr;
+
+	while(!istr_list.empty())
+	{
+		istream& istr = *istr_list.front();
+
+		int stream_pos = istr.tellg();
+		istr.seekg(0, ios::end);
+		int stream_end = istr.tellg();
+		istr.seekg(stream_pos, ios::beg);
+
+		int len = end_itr - bbuf_itr;
+		if(len > (stream_end - stream_pos) / sizeof (sample_t))
+		{
+			len = (stream_end - stream_pos) / sizeof (sample_t);
+		}
+		total_len += len;
+
+		istr.read(reinterpret_cast<char*>(&(*bbuf_itr)), len * sizeof (sample_t));
+		bbuf_itr += len;
+
+		fillsize = end_itr - bbuf_itr;
+		if(fillsize == 0)
+		{
+			//the buffer is filled
+			break;
+		}
+		//We're done with this stream
+		istr_list.pop_front();
+	}
+
+	bbuf_itr = fill_n(bbuf_itr, fillsize, 0.f);
+
+	if(bbuf_itr != end_itr)
+	{
+		throw runtime_error("bug in load_back_buffer!");
+	}
+
+	bbuf_ready = false;
+
+	return total_len;
+}
+
+int aram::service::load_and_read_buffer::load_back_buffer_and_swap(forward_list<istream*>& istr)
+{
+	int samples_read = load_back_buffer(istr);
+	swap();
+	return samples_read;
+}
+
+/*
+ *  aram::service::write_and_store_buffer
+ */
+aram::service::write_and_store_buffer::write_and_store_buffer(int siz) : double_buffer(siz)
+{
+}
+
+bool aram::service::write_and_store_buffer::write_front_buffer(sample_t* from_buf, int len)
+{
+	buf_itr_t itr;
+	{
+		lock_guard<mutex> guard(swap_safe);
+		if(fbuf_itr + len < front_buffer()->end())
+		{
+			itr = fbuf_itr;
+			fbuf_itr += len;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	copy(from_buf, &from_buf[len], &(*itr));
+
+	itr += len;
+
+	return true;
+}
+
+int aram::service::write_and_store_buffer::swap_and_store_back_buffer(ostream& ostr)
+{
+	int len = fbuf_itr - front_buffer()->begin();
+	swap();
+
+	ostr.write(reinterpret_cast<char*>(&(*back_buffer()->begin())), len * sizeof (sample_t));
+
+	return len;
+}
+
+/*
+ *  aram::service::single_buffer
+ */
+aram::service::single_buffer::single_buffer(istream& istr)
+{
+	istr.seekg(0, ios::end);
+	int len = istr.tellg() / sizeof (sample_t);
+	istr.seekg(0, ios::beg);
+
+	buf = new buf_t(len);
+	reset();
+	istr.read(reinterpret_cast<char*>(&(*buf_itr)), len * sizeof (sample_t));
+}
+
+void aram::service::single_buffer::read(sample_t* to, int len)
+{
+	if(buf_itr + len > buf->end())
+	{
+		throw "out of bounds"; //maybe better to fill w zeros?
+	}
+	copy(buf_itr, buf_itr + len, to);
+	buf_itr += len;
+}
+
+void aram::service::single_buffer::reset()
+{
+	buf_itr = buf->begin();
+}
---
+++ src/main/cc/service/buffer.h
@@ -0,0 +1,126 @@
+/*
+	ARAMv0, the minimalistic Audio Recorder And Music
+	Copyright (C) 2016 Eigil Hysvær
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARAM_BUFFERS_H
+#define ARAM_BUFFERS_H
+
+#include <array>
+#include <vector>
+#include <forward_list>
+#include <mutex>
+
+using namespace std;
+
+namespace aram
+{
+	namespace service
+	{
+		typedef float sample_t;
+		typedef vector<sample_t> buf_t;
+		typedef buf_t::iterator buf_itr_t;
+
+
+		class double_buffer
+		{
+		protected:
+			array<buf_t*, 2> dbuf;
+			buf_itr_t fbuf_itr;
+			bool bbuf_ready;
+			int b_idx;
+			mutex swap_safe;
+
+			buf_t* front_buffer();
+			buf_t* back_buffer();
+			void swap();
+
+		public:
+			/**
+			 * Constructor, allocate buffers.
+			 * @param len Length of each buffer.
+			 */
+			double_buffer(int siz);
+
+			/**
+			 * Free buffers.
+			 */
+			virtual ~double_buffer();
+		};
+
+
+		class load_and_read_buffer : public double_buffer
+		{
+		public:
+
+			load_and_read_buffer(int siz);
+
+			/**
+			 * Copy len floats to given buffer. Swaps the front/back buffers if necessary.
+			 * Note, len > buffers is not considered. This class' intention is to be used as
+			 * a large buffer for some smaller real time/hardware buffers.
+			 * Return true if there was a swap.
+			 */
+			bool read_front_buffer(sample_t* to_buf, int len);
+
+			/**
+			 * Fill up back buffer. Excess buffer space are filled with 0.
+			 * Return number of actual samples read from streams.
+			 */
+			int load_back_buffer(forward_list<istream*>& istr);
+
+			/**
+			 * Use first time so that there's data to read in the front buffer.
+			 * Return number of actual samples read from streams.
+			 */
+			int load_back_buffer_and_swap(forward_list<istream*>& istr);
+		};
+
+
+		class write_and_store_buffer : public double_buffer
+		{
+		public:
+
+			write_and_store_buffer(int siz);
+
+			/**
+			 * Write len number of samples to front buffer.istr_list
+			 * Return true if it could write all.
+			 * Note - if false it means that there are samples that never will be stored.
+			 */
+			bool write_front_buffer(sample_t* from_buf, int len);
+
+			/**
+			 * Swap the buffers and write to ostr as much as was written to it.
+			 * Return number of samples stored.
+			 */
+			int swap_and_store_back_buffer(ostream& ostr);
+		};
+
+
+		class single_buffer
+		{
+			buf_t* buf;
+			buf_itr_t buf_itr;
+
+		public:
+			single_buffer(istream& istr);
+			void read(sample_t* to, int len);
+			void reset();
+		};
+	}
+}
+#endif
